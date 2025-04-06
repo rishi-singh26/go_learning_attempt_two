1e469c311fa19c6862723e1c7d6dcee665c85ea7
rishi-singh26 <rishisingh0831@gmail.com>
2025-04-06 12:18:05 +0530

    Go types, inferred types and type conversion

---
+++ 01.go_essentials/go.mod
@@ -0,0 +1,3 @@
+module rishisingh.in/investment-calculator
+
+go 1.22.4
---
+++ 01.go_essentials/investment_calculator.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	fmt.Print(futureValue)
+}
