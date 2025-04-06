e1e3821aabbeb92898f3e39bb0233a46c4d9bdd5
rishi-singh26 <rishisingh0831@gmail.com>
2025-04-06 19:41:02 +0530

    Completed Investment Calculator

--- 01.go_essentials/investment_calculator.go
+++ 01.go_essentials/investment_calculator.go
@@ -6,9 +6,24 @@ import (
 )
 
 func main() {
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	const inflationRate = 2.5
+	var investmentAmount float64 = 1000
+	years := 10.0
+	expectedReturnRate := 5.5
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
-	fmt.Print(futureValue)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
