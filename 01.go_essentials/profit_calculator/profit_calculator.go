40ca3be05f88b23828f886b04448eb1455d7884d
rishi-singh26 <rishisingh0831@gmail.com>
2025-04-12 23:10:17 +0530

    Error handelling exercise

--- 01.go_essentials/profit_calculator/profit_calculator.go
+++ 01.go_essentials/profit_calculator/profit_calculator.go
@@ -1,28 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getInput("Revenue: ")
-	expences := getInput("Expences: ")
-	taxes := getInput("Taxes: ")
+	revenue, revenueErr := getInput("Revenue: ")
+	expences, expenseErr := getInput("Expences: ")
+	taxes, taxErr := getInput("Taxes: ")
+	if revenueErr != nil || expenseErr != nil || taxErr != nil {
+		fmt.Println(taxErr)
+		return
+	}
 
 	ebt, profit, ratio := calculateProfit(revenue, expences, taxes)
+
+	writeCalculationToFile(ebt, profit, ratio)
 
 	printOutput("EBT: ", ebt)
 	printOutput("Profit: ", profit)
 	printOutput("Ratio: ", ratio)
 }
 
-func getInput(message string) (input float64) {
+func getInput(message string) (float64, error) {
+	var input float64
 	fmt.Print(message)
 	fmt.Scan(&input)
-	return
+
+	if input <= 0 {
+		return 0, errors.New("invalid values entered")
+	}
+
+	return input, nil
 }
 
 func printOutput(message string, value float64) {
 	fmt.Print(message)
-	fmt.Println(value)
+	fmt.Printf("%.2f\n", value)
 }
 
 func calculateProfit(revenue, expences, taxes float64) (ebt, profit, ratio float64) {
@@ -31,4 +47,9 @@ func calculateProfit(revenue, expences, taxes float64) (ebt, profit, ratio float
 	ratio = ebt / profit
 
 	return
+}
+
+func writeCalculationToFile(ebt, profit, ratio float64) {
+	calculationStr := fmt.Sprintf("Earnings before tax: %.2f\nProfit: %.2f\nRatio: %.2f", ebt, profit, ratio)
+	os.WriteFile("profit_calculation.txt", []byte(calculationStr), 0644)
 }
